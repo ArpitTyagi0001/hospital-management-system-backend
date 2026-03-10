58e3d2b5b770ec6c3af3df010b546b14be4df62b
Arpit Tyagi <arpittyagi389@gmail.com>
2026-03-10 23:07:48 +0530

    Make a first entity as Patient

---
+++ src/main/java/com/example/HospitalMangement/entity/Patient.java
@@ -0,0 +1,43 @@
+package com.example.HospitalMangement.entity;
+
+import com.example.HospitalMangement.entity.type.BloodGroupType;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(
+        name = "patient"
+)
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 40)
+    private String name;
+
+    private String gender;
+
+    private LocalDate dob;
+
+    @Column(unique = true ,  nullable = false)
+    private String email;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createAt;
+
+    @Enumerated(EnumType.STRING)
+    private BloodGroupType bloodGroup;
+
+
+}
---
+++ src/main/java/com/example/HospitalMangement/entity/type/BloodGroupType.java
@@ -0,0 +1,12 @@
+package com.example.HospitalMangement.entity.type;
+
+public enum BloodGroupType {
+    A_POSITIVE,
+    A_NEGATIVE,
+    B_NEGATIVE,
+    B_POSITIVE,
+    AB_POSITIVE,
+    AB_NEGATIVE,
+    O_NEGATIVE,
+    O_POSITIVE
+}
