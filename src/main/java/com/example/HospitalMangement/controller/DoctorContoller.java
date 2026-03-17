3f1dea8f8a4d388916c055a29ecf149697dc76c5
Arpit Tyagi <arpittyagi389@gmail.com>
2026-03-17 20:22:47 +0530

    make doctor request

--- src/main/java/com/example/HospitalMangement/controller/DoctorContoller.java
+++ src/main/java/com/example/HospitalMangement/controller/DoctorContoller.java
@@ -1,4 +1,22 @@
 package com.example.HospitalMangement.controller;
 
+import com.example.HospitalMangement.dto.DoctorDto;
+import com.example.HospitalMangement.entity.Doctor;
+import com.example.HospitalMangement.service.DoctorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class DoctorContoller {
+
+    @Autowired
+    private DoctorService doctorService;
+
+    @GetMapping("/doctors")
+    public List<DoctorDto> getDoctor(){
+        return doctorService.getDoctors();
+    }
 }
--- src/main/java/com/example/HospitalMangement/service/DoctorService.java
+++ src/main/java/com/example/HospitalMangement/service/DoctorService.java
@@ -1,5 +1,31 @@
 package com.example.HospitalMangement.service;
 
+import com.example.HospitalMangement.dto.DoctorDto;
+import com.example.HospitalMangement.entity.Doctor;
+import com.example.HospitalMangement.repository.DoctorRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class DoctorService {
 
+    @Autowired
+    private DoctorRepository doctorRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public List<DoctorDto> getDoctors() {
+        List<Doctor> doctors = doctorRepository.findAll();
+
+        List<DoctorDto> doctorDtos = doctors.stream()
+                .map(doctor -> modelMapper.map(doctor , DoctorDto.class))
+                .collect(Collectors.toList());
+
+        return doctorDtos;
+    }
 }
