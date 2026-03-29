071b356df2a14da9973dddacfa656bf8c6658dab
= <arpittyagi389@gmail.com>
2026-03-29 21:47:34 +0530

    start implement jwt token

--- src/main/java/com/example/HospitalMangement/entity/UsersPatient.java
+++ src/main/java/com/example/HospitalMangement/entity/UsersPatient.java
@@ -17,7 +17,7 @@ public class UsersPatient implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections. singleton(new SimpleGrantedAuthority("USER"));
+        return Collections.singleton(new SimpleGrantedAuthority("USER"));
     }
 
     @Override
